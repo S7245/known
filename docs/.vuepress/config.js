54e4128966728deff4bcf3105e621814f8d6212a
刘山 <>
2022-08-11 08:47:50 +0800

    更新内容

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+const { defaultTheme } = require('vuepress')
+const { backToTopPlugin } = require('@vuepress/plugin-back-to-top')
+const { mediumZoomPlugin } = require('@vuepress/plugin-medium-zoom')
+const { nprogressPlugin } = require('@vuepress/plugin-nprogress')
+
+module.exports = {
+    lang: 'zh-CN',
+    title: '知识图谱',
+    port: '8087',
+    base: "/known/",
+    description: "知识图谱",
+
+    plugins: [
+        backToTopPlugin(),
+        mediumZoomPlugin(),
+        nprogressPlugin(),
+    ],
+
+    markdown: {
+        lineNumbers: true,
+    },
+
+    theme: defaultTheme({
+        logo: '/favicon.png',
+        navbar: navbarFun(),
+        //sidebar: sidebarFun(),
+    }),
+}
+
+/* 顶部导航栏 */
+function navbarFun() {
+    return [
+        { text: '首页', link: '/' },
+        { text: '营销学', link: '/marketing/' },
+        { text: '商业学', link: '/bp/' },
+    ];
+}
+
+/* 侧边菜单栏 */
+function sidebarFun() {
+    return {
+        '/marketing/': [{
+            text: '营销'
+        }],
+    };
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: /logo.jpg
+# heroText: Sam's Blog
+tagline: 
+---
+
+学习：营销学、商业模式、投资学习等
