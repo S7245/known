f6ec01ec72a68ec492754a6c5e9932e76baf9145
刘山 <>
2022-08-11 08:50:15 +0800

    更新内容

--- commit.sh
+++ commit.sh
@@ -4,5 +4,5 @@ git pull
 git add -A
 git commit -m '更新内容'
 # git push -u origin master
-git push -u git@github.com:samlau0401/known.git master
+git push -f git@github.com:samlau0401/known.git master
 # chmod 777 /commit.sh  #使脚本具有执行权限
